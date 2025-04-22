aaad212c020f07291fb8bf340146e9f81b226819
hantonette <habalajadia23-0250@cca.edu.ph>
2025-04-23 00:16:07 +0800

    Delete Balajadia_Portfolio directory

--- Balajadia_Portfolio/portfolio_js.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("contactForm").addEventListener("submit", function(event) {
-    event.preventDefault(); // Prevents page reload
-    alert("Thanks! Your message is most appreciated ❤️"); // Custom alert message
-});
